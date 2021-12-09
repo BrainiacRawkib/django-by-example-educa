cac4722d233a280808505781a356dbd4c0f601a0
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-12-08 22:50:33 -0800

    :hammer: templates & static

--- src/educa/urls.py
+++ src/educa/urls.py
@@ -1,21 +1,23 @@
-"""educa URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('login/',
+         auth_views.LoginView.as_view(extra_context={'title': 'Login'}),
+         name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(),
+         name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
+
+admin.site.site_header = 'Educa Project'
+admin.site.site_title = 'Educa Project'
+admin.site.index_title = 'Welcome Onboard!'
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
