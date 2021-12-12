c61cf5ddf3734a6267e3b5fd3f3a2f7d45554b02
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-12-11 18:01:13 -0800

    :hammer: courses:: CourseListView()

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,5 +1,6 @@
 from braces.views import CsrfExemptMixin, JSONResponseMixin
 from django.apps import apps
+from django.db.models import Count
 from django.forms.models import modelform_factory
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -7,8 +8,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from .forms import ModuleFormSet
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 
 
 class OwnerMixin(object):
@@ -171,3 +173,21 @@ class ContentOrderView(CsrfExemptMixin, JSONResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+            context = {
+                'subjects': subjects,
+                'subject': subject,
+                'courses': courses
+            }
+        return self.render_to_response(context)
