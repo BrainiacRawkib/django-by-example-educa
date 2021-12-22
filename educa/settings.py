f26217804ca6148ce8fbe830daa0950179b62dc8
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-12-21 18:10:54 -0800

    :hammer: added 'django.middleware.common.BrokenLinkEmailsMiddleware' to MIDDLEWARE

--- educa/settings.py
+++ educa/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
     # 'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
