987c95b4cfc62e0c1fba0f5b3bb173d574559ab4
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-12-21 18:33:23 -0800

    :hammer: edited educa/settings.py

--- educa/settings.py
+++ educa/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = [
-    'http://djangoeduca.herokuapp.com', 'djangoeduca.herokuapp.com'
-]
+ALLOWED_HOSTS = []
 
 ADMINS = [
     (os.getenv('ADMIN'), os.getenv('EMAIL_HOST_USER'))
